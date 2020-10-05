58ab70c73217dab8d5bdb4f253025e53083cbda8
Majkluss <Michaluss@seznam.cz>
2020-10-05 17:39:09 +0200

    update

--- PrinterMini.py
+++ PrinterMini.py
@@ -5,5 +5,11 @@ class PrinterMini(Printer):
         super().__init__(model, nozzle)
         self._connect = connect
 
+        self.supported_filaments = {
+            "PLA": 215,
+            "ABS": 220
+        }
+
     def __str__(self):
-        return(f"Tiskárna mini, model {self._model}, průměr trysky {self._nozzle}mm. Připojena pomocí {self._connect}. Verze firmware {self.firmware}")
+        filament = ", ".join(self.supported_filaments)
+        return(f"Tiskárna mini, model {self._model}, průměr trysky {self._nozzle}mm. Připojena pomocí {self._connect}. Verze firmware {self.firmware}. Podporované filamenty: {filament}")
